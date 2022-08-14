cb8aaccb2e88a2ca6fe689a7c2dfe774e5321eb7
Hyu-waseda <95074991+Hyu-waseda@users.noreply.github.com>
2022-08-14 20:39:09 +0900

    文法　終わり

--- src/index.js
+++ src/index.js
@@ -199,3 +199,27 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 //   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
 // };
 // console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if(flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if(flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+//||は左がfalseなら右を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+//&&は左がtrueなら右を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
