d66b4b0b14def45bf27dfd88cd3e4ef850f75c32
Hyu-waseda <95074991+Hyu-waseda@users.noreply.github.com>
2022-08-14 16:39:39 +0900

    map filter

--- src/index.js
+++ src/index.js
@@ -152,3 +152,33 @@
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for(let i = 0;i < nameArr.length;i++){
+//   console.log(`${i+1}番目は${nameArr[i]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, i) => console.log(`${i+1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === 'じゃけぇ') {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
